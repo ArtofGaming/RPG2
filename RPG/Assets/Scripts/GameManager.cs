3afd85dac1c3ff395a5d96c540f1b7655acb569e
ArtofGaming <54565543+ArtofGaming@users.noreply.github.com>
2021-10-09 15:29:28 -0700

    github finally stops its fit

---
+++ RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPun
+{
+    [Header("Players")]
+    public string playerPrefabPath;
+    public Transform[] spawnPoints;
+    public float respawnTime;
+    private int playersInGame;
+    public static GameManager instance;
+    public PlayerController[] players;
+
+    void Awakw()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+        if(playersInGame == PhotonNetwork.PlayerList.Length)
+        {
+            SpawnPlayer();
+        }
+    }
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabPath, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+        playerObj.GetComponent<PhotonView>().RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+}
